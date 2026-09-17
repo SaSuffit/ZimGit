febf6bb02c7bef527a4cb8fcaf7d0fc87c7df768
sasuffit <zimodasher@gmail.com>
2026-09-17 01:01:58 -0400

    NEW - TxtToSpeech C# app
    TxtToSpeech C# app (CLI, DotNETv10, Vi, nano usage, install packages CLI) 
    *AI Input for issues with CLI DotNET packages, Synthesizer and iterating through properties of the options and reviewing what I can hardcode, taught me Ssml;
    
    TO-COME:
    OOP!
    Logic
    UI -> CLI menu(?)

---
+++ TxtToSpeech/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Windows.Media.SpeechSynthesis;
+
+var voices = SpeechSynthesizer.AllVoices;
+
+/*
+foreach ( var voice in voices.GetType().GetProperties())
+{
+	Console.WriteLine($"TYPE: {voice.GetType()}");
+	foreach(var property in voice.GetType().GetProperties())
+	{
+	Console.WriteLine($"{property.Name}: {property.GetValue(voice)}");
+	}
+	Console.WriteLine();
+}
+*/
+
+string ssml = File.ReadAllText("input.txt");
+
+
+SpeechSynthesizer synth = new SpeechSynthesizer(); 
+
+synth.Voice = voices[2];
+synth.Options.AudioVolume = 0.4;
+synth.Options.SpeakingRate = 0.79;
+
+SpeechSynthesisStream stream = await synth.SynthesizeSsmlToStreamAsync(ssml);
+
+using FileStream file = File.Create("output.wav");
+
+await stream.AsStreamForRead().CopyToAsync(file);
+
+Console.WriteLine("Created output.wav");
+
